11c5515611f7f81b42eac17cdd10bd98a77f6466
chintan <chintan.tops@gmail.com>
2024-06-07 19:22:48 +0530

    templates

---
+++ 011Django/myproject/myapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path("",index,name="index"),
+    path("home/",home,name="home"),
+    path("about/",about,name="about"),
+    path("contact/",contact,name="contact")
+]
--- 011Django/myproject/myapp/views.py
+++ 011Django/myproject/myapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("Welcome")
+    return render(request,"index.html")
+
+def home(request):
+    return render(request,"home.html")
+
+
+def about(request):
+    return render(request,"about.html")
+
+
+def contact(request):
+    return render(request,"contact.html")
